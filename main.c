eca44ee052ab5d685c75580a4abe7060bca64ec1
cuterinrin <yro7045@naver.com>
2018-11-22 15:51:52 +0900

    1122ch12-4th

--- main.c
+++ main.c
@@ -5,17 +5,15 @@
 
 int main(int argc, char *argv[]) {
 
-FILE *fp = NULL;
-char input[100];
+FILE *fp;
+char input;
 int i;
 
-fp = fopen("sample.txt", "w");
+fp = fopen("sample.txt", "r");
 
-for(i=0; i<3; i++)
-{
-printf("input a word : ");
-scanf("%s", input);
-fprintf(fp, "%s\n", input);
+while((input=fgetc(fp)) != EOF)
+{	
+	putchar(input);
 }
 
 fclose(fp);
